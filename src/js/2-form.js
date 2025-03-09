737a95cff51a63feaeed1d656038d9a8865b98f2
Yurii Pasternak <yuriipasternak@icloud.com>
2025-03-09 20:24:27 +0100

    goit-js-hw-09

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('.feedback-form');
-
+const STORAGE_KEY = 'feedback-form-state';
 const input = form.elements.email;
 const textarea = form.elements.message;
 
@@ -27,4 +27,35 @@ form.addEventListener('submit', evt => {
   console.log({ email: input.value, message: textarea.value });
   localStorage.removeItem(localStorageKey);
   form.reset();
+});
+
+// об'єкт для керування станом форми
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+// Заповнюємо форму з localStorage
+Object.entries(formData).forEach(([key, value]) => {
+  form.elements[key].value = value;
+});
+//  зберігаємо в `localStorage`
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+
+  if (!email || !message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log('Form Data:', formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+  formData = {}; // Очищаємо об'єкт
 });
